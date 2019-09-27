aa18dd535522810db995ee6f9e2093608b787fac
megae <megae@DESKTOP-JPSK7FO>
2019-09-27 13:08:56 -0500

    <!-- Creación de la clase Empleado, con los getters y setters -->

---
+++ TestProjectJava/src/org/ejemplo/modelo/Empleado.java
@@ -0,0 +1,26 @@
+package org.ejemplo.modelo;
+
+public class Empleado {
+
+	private int id;
+	private String nombre;
+	
+	public static void main(String[] args) {
+		System.out.println("Ejemplo GIT");
+	}
+	
+	
+	//getters and setters
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
